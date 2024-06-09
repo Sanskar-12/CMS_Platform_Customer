f81ff7807ef8ad0edd3c909323139ee30083e2f2
Sanskar-12 <sanskarv2004@gmail.com>
2024-06-09 17:57:58 +0530

    Cart page completed

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,7 +1,10 @@
 import Currency from "@/components/currency";
 import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
+import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 const Summary = () => {
 
@@ -9,9 +12,28 @@ const Summary = () => {
     const items=useCart((state)=>state.items)
     const removeAll=useCart((state)=>state.removeAll)
 
+    useEffect(()=>{
+        if(searchParams.get("success")){
+            toast.success("Payment completed.")
+            removeAll()
+        }
+
+        if(searchParams.get("cancelled")){
+            toast.error("Something went wrong.")
+        }
+    },[searchParams,removeAll])
+
     const totalPrice=items.reduce((total,item)=>{
         return total+Number(item.price)
     },0)
+
+    const onCheckout=async()=>{
+        const response=await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`,{
+            productId:items.map((item)=>item.id)
+        })
+
+        window.location=response.data.url
+    }
 
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
